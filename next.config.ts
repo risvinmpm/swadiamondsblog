4cd42c7bf300808096029ce7ade0be66416a656a
risvinmpm <risvinmp39@gmail.com>
2025-07-18 00:12:15 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
-    ignoreDuringBuilds: true, 
+    ignoreDuringBuilds: true,
   },
   images: {
     domains: ['swadiamonds.netlify.app'],
+  },
+  env: {
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    MONGODB_URI: process.env.MONGODB_URI,
   },
 };
 
