4e507b05fe69650414cfb51cfad14dcb5a71bcea
risvinmpm <risvinmp39@gmail.com>
2025-07-18 15:01:47 +0530

    next.congig changes

--- next.config.ts
+++ next.config.ts
@@ -7,12 +7,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['swadiamonds.netlify.app'],
   },
-  env: {
-    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
-    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
-    MONGODB_URI: process.env.MONGODB_URI,
-  },
 };
 
 export default nextConfig;
